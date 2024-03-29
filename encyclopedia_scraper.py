3c38ad261701c6954226062a9fd57097461a687d
victorthedude <victortruong00@gmail.com>
2024-03-29 22:19:53 +0100

    fetch content from page link

--- encyclopedia_scraper.py
+++ encyclopedia_scraper.py
@@ -7,7 +7,7 @@ import pathlib
 
 URL = 'https://runeberg.org/nf/'
 
-def fetch_volume_links(soup: BeautifulSoup):
+def fetch_edition_volume_links(soup: BeautifulSoup):
     # Fetch the <table> element that holds the links to the volumes for each edition
     # OBS: Unfortunately it has no identifying attribute, so we just hard code it fetching the second table found in the HTML.
     edition_table = soup.find('table').find_next('table')
@@ -27,7 +27,7 @@ def fetch_volume_links(soup: BeautifulSoup):
 
     return editions_volume_links
 
-def fetch_page_links(volume_link):
+def fetch_volume_page_links(volume_link):
     volume_url = urljoin(URL, volume_link)
     response = requests.get(volume_url)
     if response.status_code != 200:
@@ -38,7 +38,7 @@ def fetch_page_links(volume_link):
         def is_page_link(tag):
             if tag.name == 'a' and tag.has_attr('href'):
                 has_page_link = bool(re.search(r'\d\d\d\d.html',   tag['href']))  # check if it links to a page
-                includes_page_numbers = bool(re.search(r'\d+-\d+', tag.text))   # check that it links to relevant encyclopedia content (i.e. no preface etc...)
+                includes_page_numbers = bool(re.search(r'\d+-\d+', tag.text))     # check that it links to relevant encyclopedia content (i.e. no preface etc...)
                 return has_page_link and includes_page_numbers
             return False
         
@@ -47,14 +47,42 @@ def fetch_page_links(volume_link):
         page_links = {tag['href'] for tag in soup.find_all(is_page_link)}
         return page_links
 
-# TODO:
-# def fetch_page_content_from_first_edition(page_link):
-#     page_url = urljoin(URL, page_link)
-#     response = requests.get(page_url)
-#     if response.status_code != 200:
-#         print(f"Failed to retrieve the webpage ({page_url}): HTTP {response.status_code}")
-#     else:
-#         soup = BeautifulSoup(response.text, 'html.parser')
+def fetch_volume_page_content(volume_link, page_link):
+    volume_url = urljoin(URL, volume_link)
+    page_url = urljoin(volume_url, page_link)
+    response = requests.get(page_url)
+    if response.status_code != 200:
+        print(f"Failed to retrieve the webpage ({page_url}): HTTP {response.status_code}")
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # 1. Define regex strings
+        #    Check for keywords listed at top of the document
+        entries_re = r"On this page \/ på denna sida\n(.*?)<<\sprev.*page\s>>"
+        #    Check for proofread statement
+        start_re = r"(?:This page has been proofread at least once.+\(historik\)|This page has never been proofread\. / Denna sida har aldrig korrekturlästs\.)"
+        #    Page content
+        content_re = r"(?:\d+\n\n.*?\n\n\d+)?(.*?)"
+        #    End tag
+        end_re = r"<<\sprev.*page\s>>"
+        regex = entries_re + r".*" + start_re + content_re + end_re
+        match = re.search(regex, soup.find('p').text, re.DOTALL)
+        # 2. Extract entries and content
+        entries_text = match.group(1).strip(' \n')
+        content_text = match.group(2).strip(' \n')
+        return entries_text, content_text
+
+def download_edition(edition_dir_path, edition_vols: dict):
+    for vol_link, page_links in edition_vols.items():
+        vol_dir = f"{edition_dir_path}\\{vol_link.strip('/')}"
+        # Create volume directory:
+        pathlib.Path(vol_dir).mkdir(exist_ok=False)
+        for page_link in page_links:
+            filename = page_link.strip('.html') + ".txt"
+            # Fetch the text content of each page and save locally to text files within each volume:
+            # TODO
+            # text = fetch_volume_page_content(vol_link, page_link)
+            # with open(f"{vol_dir}\\{filename}", mode='x') as f:
+            #     f.write(text)
 
 def main():
     base_dir = os.path.dirname(os.path.realpath(__file__))
@@ -74,28 +102,23 @@ def main():
         # Parse the HTML content of 'https://runeberg.org/nf/'
         soup = BeautifulSoup(response.text, 'html.parser')
         # 1. Each edition contains a number of volumes, each with it's own hyperlink:
-        edition_vol_links = fetch_volume_links(soup)
+        edition_vol_links = fetch_edition_volume_links(soup)
 
-        # 2. Fetch the pages of all volumes for first and fourth edition and collect in dict:
-        first_edition = {}
-        fourth_edition = {}
+        # 2. Fetch the pages of all volumes for first and fourth edition and collect in dicts:
+        first_edition_vols = {}
+        fourth_edition_vols = {}
         for vol_links in edition_vol_links:
             first_ed_vol_link = vol_links[0]
             fourth_ed_vol_link = vol_links[2]
             if first_ed_vol_link:
-                first_edition[first_ed_vol_link] = fetch_page_links(first_ed_vol_link)
+                first_edition_vols[first_ed_vol_link] = fetch_volume_page_links(first_ed_vol_link)
             if fourth_ed_vol_link:
-                fourth_edition[fourth_ed_vol_link] = fetch_page_links(fourth_ed_vol_link)
+                fourth_edition_vols[fourth_ed_vol_link] = fetch_volume_page_links(fourth_ed_vol_link)
 
         # 3. Create local folder structure for saving encyclopedia locally
         #    Create volume directories:
-        for vol, page_links in first_edition.items():
-            vol_dir = vol.strip('/')
-            pathlib.Path(f"{first_edition_path}\\{vol_dir}").mkdir(exist_ok=False)
-            # TODO:
-            # for link in page_links:
-            #     # Fetch the text content of each page and save locally to text file:
-            #     filename = link.strip('.html') + ".txt"
+        download_edition(first_edition_path, first_edition_vols)
+        download_edition(fourth_edition_path, fourth_edition_vols)
         
 
 if __name__ == "__main__":
