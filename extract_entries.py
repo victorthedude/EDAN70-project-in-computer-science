24c51286746636ccda1887fc15d3f893469d34aa
victorthedude <victortruong00@gmail.com>
2024-04-16 16:53:46 +0200

    modify scoring heuristics for headword selection: use 'candidate' approach + lower min similarity score to 0.6

--- extract_entries.py
+++ extract_entries.py
@@ -19,7 +19,7 @@ def get_headword_from_index(text: str, index: list[str]):
         headword_count = len(headword.split(' '))
         headword = headword.strip(' ,.')
         first_words = [word.strip(' ,.') for word in re.sub("\s+"," ",text).split(' ')[:2+headword_count]]
-        if headword_count > 1: # If headword  consists of more than one word:
+        if headword_count > 1: # If headword consists of more than one word:
             for i in range(0,len(first_words)-headword_count):
                 # if i+headword_count > len(headword_count):
                 #     break
@@ -36,26 +36,32 @@ def get_headword_by_score(text, index):
     text = re.sub(r"[.,'\s]", "", remove_tags(text))
 
     scores = [0 for _ in range(len(index))]
+    headword_candidates = []
     for i, headword in enumerate(index):
         # "normalize" headword:
-        headword = re.sub(r"[.,'\s]", "", headword)
-        sim_score = levenshtein_ratio(headword, text[:len(headword)]) 
-        if sim_score == 1:
-            print(f"Found '{headword}' for '{text}' with {sim_score}")
-            return headword
+        headword_norm = re.sub(r"[.,'\s]", "", headword)
+        sim_score = levenshtein_ratio(headword_norm, text[:len(headword_norm)]) 
+        if sim_score >= (len(headword_norm)-1) / len(headword_norm): # one char error/edit ratio
+            # print(f"Found '{headword}' for '{text}' with {sim_score}")
+            headword_candidates.append(headword)
         else:
             scores[i] = sim_score
+    if headword_candidates:
+        # heuristic: if we find multiple headword candidates, choose the longest headword.
+        best = max(headword_candidates, key=len)
+        # print(f"  Chose '{best}' for '{text}'")
+        return best
 
     headword = None
     best_score = 0
-    # If nothing else; go for highest reasonable (>=0.8) similarity score:
+    # If nothing else; go for highest reasonable (>=0.6) similarity score:
     for i in range(len(scores)):
         score = scores[i]
-        if score >= 0.8 and score > best_score:
+        if score >= 0.6 and score > best_score: # '0.6' is usually considered "close enough"
             best_score = score
             headword = index[i]
-            print(f"Found '{headword}' for '{text}' with {best_score}")
-    # if headword:
+            # print(f"Found '{headword}' for '{text}' with {best_score}")
+
     return headword
 
 def remove_tags(string):
@@ -85,7 +91,7 @@ def extract_entries_from_page(page_path, current_entry_nbr, volume_nbr, edition)
     index, content = get_page_index_and_content(page_path)
     entries = []
     paragraphs = [paragraph.strip('\n') for paragraph in content.split('\n\n')]
-    print(f"INDEX: {index}")
+    # print(f"INDEX: {index}")
     for paragraph in paragraphs:
         if not paragraph: # some documents can include more newlines than usual which results in "empty" paragarphs
             continue
@@ -113,7 +119,6 @@ def extract_entries_from_page(page_path, current_entry_nbr, volume_nbr, edition)
         current_entry_nbr += 1
 
         text = text.replace('\n', ' ')
-        # print(f"'{headword}' for '{text}' ")
 
     return entries, current_entry_nbr
 
@@ -123,7 +128,9 @@ if __name__ == '__main__':
     test_3 = 'data\\nf_first_edition\\nfaa\\0024.txt'  # straight-forward; complete index + bold tags
     test_4 = 'data\\nf_first_edition\\nfaa\\1299.txt'  # complete index but has a bunch of character errors in text. no bold-tags.
 
-    test = f'{FIRST_ED}\\nfaa\\1525.txt'
+    test = f'{FIRST_ED}\\nfaa\\1525.txt' # Bank
+    # test = f'{FOURTH_ED}\\nffa\\0165.txt'
+    # test = f'{FIRST_ED}\\nffr\\0019.txt' # Richert family
 
     entries, _ = extract_entries_from_page(test, 1, 1, 1)
     with open("sample.json", "w", encoding='utf-8') as outfile: 
